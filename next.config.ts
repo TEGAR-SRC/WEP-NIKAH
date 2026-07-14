4c1e2066b545a91e0e8a91880c33682cd8d6b840
unknown <tegararrahman17@gmail.com>
2026-07-14 22:49:46 +0700

    fix next-on-pages config + build script

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -8,7 +9,12 @@ const nextConfig: NextConfig = {
     "@whiskeysockets/baileys",
     "sharp",
     "jimp",
+    "pino",
   ],
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
