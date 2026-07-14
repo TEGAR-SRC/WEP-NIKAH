9792e4c47255b7ff2dece7cf6374edc659e2f9b0
unknown <tegararrahman17@gmail.com>
2026-07-14 22:51:18 +0700

    fix next.config - remove top-level await

--- next.config.ts
+++ next.config.ts
@@ -12,8 +12,4 @@ const nextConfig: NextConfig = {
   ],
 };
 
-if (process.env.NODE_ENV === "development") {
-  await import("@cloudflare/next-on-pages/next-dev").then((m) => m.setupDevPlatform());
-}
-
 export default nextConfig;
